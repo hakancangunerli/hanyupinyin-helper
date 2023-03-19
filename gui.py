fd9a0f47b396706702e97ee89d38ebbf0fb59995
Hakan Can Gunerli <hakancangunerli@gmail.com>
2023-03-19 00:15:23 -0400

    Master updated the gui and get rid of the modularity.

--- gui.py
+++ gui.py
@@ -1,17 +1,21 @@
 # streamlit interface 
 import streamlit as st
-from main import main
-from eng2mand import english_to_chinese
-from mand2eng import chinese_to_english
-
-
+from pypinyin import pinyin
+from translate import Translator
+    
 st.title("English to Chinese and Chinese to English")
 
 # create an input box for the user to enter the text to be translated.
 user_input = st.text_input("Enter the text to be translated:")
-buttonclick = st.button("English to Pinyin")
 
-if buttonclick:
-    st.write(main(user_input,"english"))
-    st.audio("output.mp3")
-    
+eng_to_pinyin = st.button("English to Pinyin")
+
+if eng_to_pinyin:
+    # translate user input into mandarin, using the translate module
+    translator = Translator(to_lang="zh")
+    translation = translator.translate(user_input)
+    st.write(translation)
+
+    pis = pinyin(translation, heteronym=True)
+    for i in range(len(pis)):
+        st.write(pis[i])  # print the pinyin characters one by one
--- 翻译.py
+++
@@ -1,6 +0,0 @@
-from translate import Translator
-def 英语(user_input, lang_from, lang_to):
-    translator = Translator(from_lang=lang_from, to_lang=lang_to)
-    translation = translator.translate(user_input)
-    print(translation)
-    return translation
